32add2c11d9f72b327f542aaae13f73212ae6e27
Jeffrey <jsmith6690@gmail.com>
2018-12-26 01:01:59 -0500

    update deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cd ./dist/apps/jefftopia && zip ./dist.zip -r * && cd ../../..
-mv ./dist/apps/jefftopia/dist.zip ./dist.zip
-# aws s3 cp ./dist.zip $BUCKET --acl public-read
+# cd ./dist/apps/jefftopia && zip ./dist.zip -r * && cd ../../..
+# mv ./dist/apps/jefftopia/dist.zip ./dist.zip
+source .env && aws s3 cp ./dist/apps/jefftopia $BUCKET --recursive --acl public-read --profile jeff
